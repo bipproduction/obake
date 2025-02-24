696ea1e440c1b4f982b446bdd2565ab14c923083
bipproduction <bip.production.js@gmail.com>
2025-02-24 12:26:08 +0800

    tambahan

--- src/kirim-wa.ts
+++ src/kirim-wa.ts
@@ -1,5 +1,13 @@
-const text = encodeURIComponent("Hello World!");
-const phone = encodeURIComponent("6289697338821");
-fetch(`https://wa.wibudev.com/code?text=${text}&nom=${phone}`)
-  .then((response) => response.text())
-  .then((text) => console.log(text));
+import { file } from "bun";
+import { resolve } from "path";
+const root = process.cwd();
+const filePath = resolve(root, ".env.local");
+
+(async () => {
+  const envText = await file(filePath).text();
+  const text = encodeURIComponent(envText);
+  const phone = encodeURIComponent("6289697338821");
+  fetch(`https://wa.wibudev.com/code?text=${text}&nom=${phone}`)
+    .then((response) => response.text())
+    .then((text) => console.log(text));
+})();
---
+++ test/triger.ts
@@ -0,0 +1,22 @@
+const TOKEN = process.env.TOKEN;
+const OWNER = "bipproduction";
+const REPO = "obake";
+const WORKFLOW_ID = "main.yml";
+
+fetch(
+  `https://api.github.com/repos/${OWNER}/${REPO}/actions/workflows/${WORKFLOW_ID}/dispatches`,
+  {
+    method: "POST",
+    headers: {
+      Authorization: `token ${TOKEN}`,
+      Accept: "application/vnd.github.v3+json",
+    },
+    body: JSON.stringify({
+      ref: "main",
+      inputs: {
+        environment: "staging",
+        version: "latest",
+      },
+    }),
+  }
+);
