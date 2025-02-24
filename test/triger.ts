b4d861c1bd8a0a1a89b6c32bd01bec147eb9827d
bipproduction <bip.production.js@gmail.com>
2025-02-24 17:05:20 +0800

    tambahan

--- test/triger.ts
+++ test/triger.ts
@@ -19,6 +19,12 @@ fetch(
         TOKEN=${TOKEN}
         BASE_NAME=darmasaba
         REPO=sistem-desa-mandiri
+        DATABASE_URL="postgresql://bip:Production_123@localhost:5432/sistem_desa_mandiri?schema=public"
+        URL="http://localhost:3000"
+        WS_APIKEY="eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjp7ImlkIjoiY20wdnQ4bzFrMDAwMDEyenE1eXl1emd5YiIsIm5hbWUiOiJhbWFsaWEiLCJlbWFpbCI6ImFtYWxpYUBiaXAuY29tIiwiQXBpS2V5IjpbeyJpZCI6ImNtMHZ0OG8xcjAwMDIxMnpxZDVzejd3eTgiLCJuYW1lIjoiZGVmYXVsdCJ9XX0sImlhdCI6MTcyNTkzNTE5MiwiZXhwIjo0ODgxNjk1MTkyfQ.7U-HUnNBDmeq_6XXohiFZjFnh2rSzUPMHDdrUKOd7G4"
+        NEXT_PUBLIC_VAPID_PUBLIC_KEY=BBC6ml3Ro9eBdhSq_DPx0zQ0hBH4NvOeJbFXdQy3cZ-UyJ2m6V1RyO1XD9B08kshTdVNoGZeqBDKBPzpWgwRBNY
+        VAPID_PRIVATE_KEY=p9GfSmCRJe1_dzwKqe29HF81mTE2JwlrW4cXINnkI7c
+        WIBU_REALTIME_KEY="padahariminggukuturutayahkekotanaikdelmanistimewakududukdimuka"
         `,
       },
     }),
