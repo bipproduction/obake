cd9671c8c6a02300e59e697a486979c6cd2ed226
bipproduction <bip.production.js@gmail.com>
2025-02-26 12:12:04 +0800

    tambahan

--- index.ts
+++ index.ts
@@ -1,9 +1,8 @@
+import { file } from "bun";
 import minimist from "minimist";
 import path from "path";
-import fs from "fs";
-import { file } from "bun";
 const argv = minimist(process.argv.splice(2));
 const root = path.resolve();
 const dataLocal = await file(path.resolve(root, "assets/data-local.json")).json();
-console.log(JSON.stringify(dataLocal), null, 2);
+console.log(JSON.stringify(dataLocal, null, 2));
 
