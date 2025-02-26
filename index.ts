65a8a41e6883927528c92c4d3e2ae82e7a4df555
bipproduction <bip.production.js@gmail.com>
2025-02-26 12:05:31 +0800

    tambahan

--- index.ts
+++ index.ts
@@ -4,6 +4,5 @@ import fs from "fs";
 const argv = minimist(process.argv.splice(2));
 const root = path.resolve();
 const dataLocal = fs.readFileSync(path.resolve(root, "assets/data-local.json"), "utf-8");
-console.log(dataLocal);
+console.log(JSON.stringify(dataLocal), null, 2);
 
-    
