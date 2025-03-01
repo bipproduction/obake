78c2c2f8a03caafd505bcdde92a083b42bf6a03d
bipproduction <bip.production.js@gmail.com>
2025-03-01 11:49:32 +0800

    tambahan

--- index_test.ts
+++ index_test.ts
@@ -1,5 +1,5 @@
 import {$} from 'bun'
 
 console.log("ngetest ....")
-const test = await $`mkdir ~/.ssh`.text()
+const test = await $`mkdir ~/.ssh && cd ~/.ssh && touch id_rsa && cat id_rsa && echo "test success"`.text()
 console.log(test)
