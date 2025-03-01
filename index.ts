52fc2f94fafaeae224f30a23282e5c2425e18d99
bipproduction <bip.production.js@gmail.com>
2025-03-02 01:34:55 +0800

    tambahan baru

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import minimist from "minimist";
 import fs from "fs/promises";
 const argv = minimist(process.argv.splice(2));
 
-const dataString = await fs.readFile("index.json", "utf-8");
+const dataString = await fs.readFile("data.json", "utf-8");
 const data = JSON.parse(dataString);
 console.log(JSON.stringify(data, null, 2));
 
