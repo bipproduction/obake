40d3e1ff449d8c1930f9410d273eb0a64c1c3cdf
bipproduction <bip.production.js@gmail.com>
2025-03-02 01:37:54 +0800

    tambahan baru

--- index.ts
+++ index.ts
@@ -3,4 +3,5 @@ import fs from "fs/promises";
 const argv = minimist(process.argv.splice(2));
 
 const dataString = await fs.readFile("data.json", "utf-8");
-console.log(dataString);
+const data = JSON.parse(dataString);
+console.log(JSON.stringify(data, null, 2));
