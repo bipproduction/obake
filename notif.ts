b06f97d14c46f1b2f6ca32dcaf1466b44fe8315c
bipproduction <bip.production.js@gmail.com>
2025-03-03 23:20:45 +0800

    tambahan baru

--- notif.ts
+++ notif.ts
@@ -1,75 +1,78 @@
 #!/usr/bin/env bun
-import dotenv from "dotenv";
-import admin from "firebase-admin";
-import fs from "fs/promises";
-import path from "path";
-import type { DataRequired } from "./init-data";
-import minimist from "minimist";
 import CryptoJS from "crypto-js";
+import admin from "firebase-admin";
+import minimist from "minimist";
+import type { DataRequired } from "./init-data";
 
 const argv = minimist(process.argv.splice(2));
-const finish = argv.finish;
+
 const data = argv.data;
-const firebase = argv.firebase;
-const token = argv.token;
 
-if (!data) {
-  console.error("data not found");
-  process.exit(1);
-}
-
-if (!firebase) {
-  console.error("firebase not found");
-  process.exit(1);
-}
-
-if (!token) {
-  console.error("token not found");
-  process.exit(1);
-}
+console.log(data)
 
 
-const dataAppJsonString = CryptoJS.AES.decrypt(data, token).toString(
-  CryptoJS.enc.Utf8
-);
+// const finish = argv.finish;
+// const data = argv.data;
+// const firebase = argv.firebase;
+// const token = argv.token;
 
-const dataAppJson: DataRequired["dataApp"] = JSON.parse(dataAppJsonString);
+// if (!data) {
+//   console.error("data not found");
+//   process.exit(1);
+// }
 
-const decryptedFirebase = CryptoJS.AES.decrypt(firebase, token).toString(
-  CryptoJS.enc.Utf8
-);
+// if (!firebase) {
+//   console.error("firebase not found");
+//   process.exit(1);
+// }
 
-const firebaseConfig = JSON.parse(decryptedFirebase);
-const app = admin.initializeApp({
-  credential: admin.credential.cert(firebaseConfig.credential),
-  databaseURL: firebaseConfig.databaseURL,
-});
+// if (!token) {
+//   console.error("token not found");
+//   process.exit(1);
+// }
 
-const db = app.database();
 
-async function kirimNotify(...args: any[]) {
-  const body = args.join(" ");
-  db.ref("/logs").child(dataAppJson.namespace).child("log").push(body);
-  console.log(body);
-}
+// const dataAppJsonString = CryptoJS.AES.decrypt(data, token).toString(
+//   CryptoJS.enc.Utf8
+// );
 
-let inputData = "";
-if (!process.stdin.isTTY) {
-  process.stdin.on("data", (chunk) => {
-    inputData += chunk.toString();
-  });
+// const dataAppJson: DataRequired["dataApp"] = JSON.parse(dataAppJsonString);
 
-  process.stdin.on("end", () => {
-    console.log("Piped input:", inputData.trim());
-    kirimNotify("[INFO]".padEnd(10, " "), inputData.trim());
-    setTimeout(() => {
-      db.app.delete();
-    }, 3000);
-  });
-} else {
-  console.log("No piped input detected.");
-}
+// const decryptedFirebase = CryptoJS.AES.decrypt(firebase, token).toString(
+//   CryptoJS.enc.Utf8
+// );
 
-if (finish) {
-  db.app.delete();
-}
+// const firebaseConfig = JSON.parse(decryptedFirebase);
+// const app = admin.initializeApp({
+//   credential: admin.credential.cert(firebaseConfig.credential),
+//   databaseURL: firebaseConfig.databaseURL,
+// });
+
+// const db = app.database();
+
+// async function kirimNotify(...args: any[]) {
+//   const body = args.join(" ");
+//   db.ref("/logs").child(dataAppJson.namespace).child("log").push(body);
+//   console.log(body);
+// }
+
+// let inputData = "";
+// if (!process.stdin.isTTY) {
+//   process.stdin.on("data", (chunk) => {
+//     inputData += chunk.toString();
+//   });
+
+//   process.stdin.on("end", () => {
+//     console.log("Piped input:", inputData.trim());
+//     kirimNotify("[INFO]".padEnd(10, " "), inputData.trim());
+//     setTimeout(() => {
+//       db.app.delete();
+//     }, 3000);
+//   });
+// } else {
+//   console.log("No piped input detected.");
+// }
+
+// if (finish) {
+//   db.app.delete();
+// }
