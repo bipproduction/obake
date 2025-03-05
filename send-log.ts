29825daeb2185676f98329f209c6ec62a2222e23
bipproduction <bip.production.js@gmail.com>
2025-03-05 10:48:41 +0800

    tambahan baru

---
+++ send-log.ts
@@ -0,0 +1,46 @@
+#!/usr/bin/env bun
+
+import CryptoJS from "crypto-js";
+import admin from "firebase-admin";
+import fs from "fs/promises";
+import minimist from "minimist";
+
+const argv = minimist(process.argv.slice(2));
+
+const data = argv.data;
+const filePath = argv["file-path"];
+
+if (!data) {
+  console.error("data not found");
+  process.exit(1);
+}
+
+if (!filePath) {
+  console.error("filePath not found");
+  process.exit(1);
+}
+
+const listData = data.split("[x]");
+const key = listData[0];
+const firebase = listData[1];
+const appData = listData[2];
+
+const dcryptFirebase = CryptoJS.AES.decrypt(firebase, key).toString(
+  CryptoJS.enc.Utf8
+);
+const dcryptAppData = CryptoJS.AES.decrypt(appData, key).toString(
+  CryptoJS.enc.Utf8
+);
+
+const firebaseJson = JSON.parse(dcryptFirebase);
+const appDataJson = JSON.parse(dcryptAppData);
+
+const app = admin.initializeApp({
+  credential: admin.credential.cert(firebaseJson.credential),
+  databaseURL: firebaseJson.databaseURL,
+});
+
+const db = app.database();
+
+const dataLog = await fs.readFile(filePath, "utf-8");
+db.ref("/logs").child(appDataJson.namespace).child("log").push(dataLog);
