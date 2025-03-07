5ef0cc82fcd2598b2c6ad43e2e4a9de932cc390a
bipproduction <bip.production.js@gmail.com>
2025-03-07 12:11:28 +0800

    tambahan baru

--- actions/ssh.ts
+++ actions/ssh.ts
@@ -1,5 +1,6 @@
 import minimist from "minimist";
 import { $ } from "bun";
+import fs from "fs/promises";
 
 const args = minimist(process.argv.slice(2));
 const data = args.data.split("[x]");
@@ -7,11 +8,9 @@ const user = data[0];
 const host = data[1];
 const key = data[2];
 
-const log = await $`
-cat <<EOF > ~/.ssh/id_rsa
-${key}
-EOF
-chmod 600 ~/.ssh/id_rsa
-`;
+await fs.writeFile("~/.ssh/id_rsa", key);
+await fs.writeFile("~/.ssh/id_rsa.pub", key);
+await $`chmod 600 ~/.ssh/id_rsa`;
+await $`chmod 600 ~/.ssh/id_rsa.pub`;
 
-console.log(log.text());
+console.log(await fs.readFile("~/.ssh/id_rsa", "utf-8"));
