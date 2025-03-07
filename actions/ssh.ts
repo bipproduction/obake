7fb373bcff9b2eae04322a80d4cb6252e0db52b8
bipproduction <bip.production.js@gmail.com>
2025-03-07 12:24:47 +0800

    tambahan baru

--- actions/ssh.ts
+++ actions/ssh.ts
@@ -14,7 +14,7 @@ await $`chmod 600 ./id_rsa`;
 await $`chmod 600 ./id_rsa.pub`;
 
 try {
-    const log = await $`ssh -i ./id_rsa ${user}@${host} -t "tree -L 3"`.text();
+    const log = await $`ssh -i ./id_rsa root@wibudev.com -t "tree -L 3"`.text();
     console.log(log);
 } catch (error) {
     console.log(error);
