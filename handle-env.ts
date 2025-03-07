d8bba2ef94429ab6bb9935b1ff3af5c986d48350
bipproduction <bip.production.js@gmail.com>
2025-03-07 21:43:08 +0800

    tambahan baru

--- handle-env.ts
+++ handle-env.ts
@@ -39,6 +39,8 @@ const appDataJson = JSON.parse(dcryptAppData);
 ;(async () => {
   const listKey = Object.keys(appDataJson);
   for (const key of listKey) {
-    await $`echo "WIBU_${_.snakeCase(_.upperCase(key))}=${appDataJson[key]}" >> $GITHUB_ENV`;
+    const dataEnv = `WIBU_${_.snakeCase(_.upperCase(key))}=${appDataJson[key]}`;
+    await $`echo "${dataEnv}" >> $GITHUB_ENV`;
+    console.log(dataEnv);
   }
 })();
