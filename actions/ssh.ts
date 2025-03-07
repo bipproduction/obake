acf32fea7cbb764c5a7341bb768c2b29a5e5468a
bipproduction <bip.production.js@gmail.com>
2025-03-07 14:30:47 +0800

    tambahan baru

--- actions/ssh.ts
+++ actions/ssh.ts
@@ -8,7 +8,7 @@ const user = data[0];
 const host = data[1];
 
 try {
-    const log = await $`ssh -i ./id_rsa root@wibudev.com -t "tree -L 3"`.text();
+    const log = await $`ssh -i ./id_rsa root@wibudev.com "echo 'Hello World'"`.text();
     console.log(log);
 } catch (error) {
     console.log(error);
