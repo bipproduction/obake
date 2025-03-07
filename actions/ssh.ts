bffa9b30c17754085d111c786a891fb6863115c0
bipproduction <bip.production.js@gmail.com>
2025-03-07 14:23:34 +0800

    tambahan baru

--- actions/ssh.ts
+++ actions/ssh.ts
@@ -6,12 +6,6 @@ const args = minimist(process.argv.slice(2));
 const data = args.data.split("[x]");
 const user = data[0];
 const host = data[1];
-const key = data[2];
-
-await fs.writeFile(`./id_rsa`, key);
-await fs.writeFile(`./id_rsa.pub`, key);
-await $`chmod 600 ./id_rsa`;
-await $`chmod 600 ./id_rsa.pub`;
 
 try {
     const log = await $`ssh -i ./id_rsa root@wibudev.com -t "tree -L 3"`.text();
